d086ff3cdf330b5710f85676aa1fbdf517c8fa86
Marcos Paulo Guedes <globalideiasml@gmail.com>
2020-05-08 10:45:12 -0300

    Primeiro Commit

---
+++ How to Install the Latest GCC on Windows_files/comment-func.php
@@ -0,0 +1,1 @@
+IDC.load_js( 'https://r-login.wordpress.com/remote-login.php?action=script_redirect&url_hash=579018ebad09283ed28fcdd4a5f63388&id=intensedebate&url=https%3A%2F%2Fwww.intensedebate.com%2Fidc%2Fjs%2Fcomment-func.php%3Fblogpostid%3D346906743%26token%3DgAjcB0hP5zmtpjK210uEQEpjhfu4ljJf%26return%3Dtrue' );
